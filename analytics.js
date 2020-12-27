e18a7f6ff2b9b08ce50f9a5aa291613231200c07
big-news <fyh516@126.com>
2020-12-27 17:55:23 +0800

    Site updated: 2020-12-27 17:55:13

--- analytics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d2bd007ca786af68050dc4eaf58eecdc";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
