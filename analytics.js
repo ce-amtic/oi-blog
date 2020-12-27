afbc9b25be6001c7943c7512dd7d9d612c5d2ca3
big-news <fyh516@126.com>
2020-12-27 17:11:53 +0800

    Site updated: 2020-12-27 17:11:47

---
+++ analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d2bd007ca786af68050dc4eaf58eecdc";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
